721edd2ea79ad6bc6b2e76ebeb8c1a184e2188a1
Your Name <you@example.com>
2024-08-06 06:01:48 +0000

    Update run_dockaka.sh to use -pbkdf2 for decryption

--- run_dockaka.sh
+++ run_dockaka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Giải mã nội dung của file dockaka.sh.enc
-openssl enc -aes-256-cbc -d -in dockaka.sh.enc -out dockaka.sh -k "your_password"
+openssl enc -aes-256-cbc -d -in dockaka.sh.enc -out dockaka.sh -k "your_password" -pbkdf2
 
 # Chạy file dockaka.sh
 bash dockaka.sh
